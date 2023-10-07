373d0d7d25840adff19e12d4b7b659e19077bb5b
shifanafin <ctshifanachennara@gmail.com>
2023-10-07 12:13:16 +0400

    part 1 working properly and i tried it with Thunder cliemd

--- Part_1/controller/blogController.js
+++ Part_1/controller/blogController.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const slugify = require('slugify');
 const sharp = require('sharp'); 
-
+// Taking the datas from  blogs.json
 const blogsFilePath = path.join(__dirname, '../blogs.json');
 let blogsData = [];
+
+// Changing the file to format
 
 try {
     blogsData = JSON.parse(fs.readFileSync(blogsFilePath, 'utf8'));
@@ -12,22 +14,23 @@ try {
     console.error('Error reading blogs data:', error);
 }
 
-
+//  Adding Datas
 const addBlogPost = async (req, res) => {
     const { title, description, main_image, additional_images, date_time } = req.body;
-    
+    // title (Min 5 characters, Max 50 characters, No special characters, REQUIRED) and
     if (!title || title.length < 5 || title.length > 50 || /[^a-zA-Z0-9\s]/.test(title)) {
       slugify(title, { lower: true })
         return res.status(400).json({ error: 'Invalid title' });
     }
-
+        //   description (Max 500 characters, REQUIRED)
     if (!description || description.length > 500) {
         return res.status(400).json({ error: 'Invalid description' });
     }
-
+    //  main_image (ONLY jpg, MAX size 1MB, REQUIRED)
     if (!main_image || !/\.(jpeg|jpg)$/.test(main_image) || main_image.length > 1048576) {
         return res.status(400).json({ error: 'Invalid main_image' });
     }
+    // additional_images (ONLY jpg, Multiple, MAX size 1MB per image, MAX number of images 5, OPTIONAL)
 
     if (additional_images) {
         if (!Array.isArray(additional_images) || additional_images.length > 5) {
@@ -35,17 +38,18 @@ const addBlogPost = async (req, res) => {
         }
 
         for (const image of additional_images) {
-          if (!/\.(jpeg|jpg)$/.test(image) || image.length > 1048576) {
+          if (!/\.(jpg)$/.test(image) || image.length > 1048576) {
             return res.status(400).json({ error: `Invalid additional_images extension ${image}` });
         }
         }
     }
+// date_time (should be unix time, and not before now, REQUIRED)
 
     if (!date_time || date_time <= Math.floor(Date.now() / 1000)) {
         return res.status(400).json({ error: 'Invalid date_time' });
     }
 
-   
+//    Compresses the images by 25%
     const compressImage = async (image) => {
         const compressedImage = await sharp(image)
             .resize({ fit: 'inside', width: 800, height: 800 })
@@ -53,7 +57,7 @@ const addBlogPost = async (req, res) => {
         return compressedImage;
     }
 
-    
+    // Saves images in "images/" folder
     const saveImage = async (image, fileName) => {
         fs.writeFileSync(`images/${fileName}`, image, 'binary');
     }
@@ -68,20 +72,22 @@ const addBlogPost = async (req, res) => {
         );
         const additionalImageFileNames = compressedAdditionalImages.map((_, index) => `additional_image_${index}_${Date.now()}.jpg`);
         await Promise.all(additionalImageFileNames.map(async (fileName, index) => await saveImage(compressedAdditionalImages[index], fileName)));
-
+    //    dds reference number incremented from last blog post in "blogs.json"
         const reference = (parseInt(blogsData[blogsData.length - 1].reference) + 1).toString().padStart(5, '0');
-        
+        //  Adds "title_slug" which is a slugified version of the title (ex: My Blog Post -> my_blog_post)
         const title_slug = slugify(title, { lower: true });
 
         const newBlogPost = {
             reference,
-            title:title_slug,
+            title,
             description,
             main_image: `images/${mainImageFileName}`,
             additional_images: additionalImageFileNames.map(fileName => `images/${fileName}`),
             date_time,
+            title_slug
            
         };
+        // Saves the blog post in "blogs.json" file
 
         blogsData.push(newBlogPost);
 
@@ -94,18 +100,22 @@ const addBlogPost = async (req, res) => {
     }
 }
 
+// Reads all blog posts from "blogs.json"
+
 const getAllBlogPosts = (req, res) => {
+    // Formats "date_time" from unix time stamp into ISO strin
   const isoDateStringToUnixTimestamp = (isoString) => {
     const date = new Date(isoString);
     return Math.floor(date.getTime() / 1000);
   }
+//   Returns blog posts
   const formattedBlogsData = blogsData.map(blog => {
     return {
       ...blog,
       date_time: isoDateStringToUnixTimestamp(blog.date_time),
     };
   });
-
+    
     return res.json(formattedBlogsData);
 }
 
--- Part_1/controller/tokenController.js
+++ Part_1/controller/tokenController.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
-
+//  Generate timed token for images API
 const generateImageToken=(req, res)=> {
+  // The API should accept these inputs:1) image_path
+
     const { image_path } = req.body;
     try{
-
+// Generates a token that will expire after 5 minutes for this exact image_path
     const token = jwt.sign({ image_path }, 'secret_key', { expiresIn: '5m' });
 
     return res.json({ token });
@@ -15,7 +17,12 @@ const generateImageToken=(req, res)=> {
 
   }
 }
+/**## Get image by token API
 
+The API should accept these inputs:
+1) image_path
+2) token
+**/
 const getImageByToken = (req, res) => {
   const { image_path, token } = req.body;
 
@@ -29,7 +36,8 @@ const getImageByToken = (req, res) => {
 
       
       const imageFilePath = path.join(__dirname, '..', image_path);
-
+/** 1) Checks if token is valid (not expired, and is made for the input "image_path"), if invalid then send back an error
+2) Sends back the image in a way that browser can display the image (not download it)**/
  
       if (!fs.existsSync(imageFilePath)) {
           return res.status(404).json({ error: 'Image not found' });
